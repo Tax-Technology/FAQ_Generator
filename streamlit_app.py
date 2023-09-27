0212eed3a6156eb16bf850a52e3ae116a14ac364
Tax-Technology <138007036+Tax-Technology@users.noreply.github.com>
2023-09-27 12:41:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,47 +22,54 @@ def main():
 
 # Define a function to parse the text and formulate questions and answers
 def parse_text_to_faq(text):
-  """Parses the given text and formulates questions and answers using generative AI.
+    """Parses the given text and formulates questions and answers using generative AI.
 
-  Args:
-    text: A string containing the text to be parsed.
+    Args:
+        text: A string containing the text to be parsed.
 
-  Returns:
-    A list of dictionaries, where each dictionary contains a question and its
-    corresponding answer.
-  """
+    Returns:
+        A list of dictionaries, where each dictionary contains a question and its
+        corresponding answer.
+    """
 
-  # Split the text into paragraphs
-  paragraphs = text.split("\n\n")
+    # Split the text into paragraphs
+    paragraphs = text.split("\n\n")
 
-  # Generate questions and answers for each paragraph
-  qa_pairs = []
-  for paragraph in paragraphs:
-    questions = qa_pipeline(paragraph)
-    for question in questions:
-      if "question" in question and "answer" in question:
-        answer = qa_pipeline(question["answer"], paragraph)["answer"]
-        qa_pairs.append({"question": question["question"], "answer": answer})
+    # Generate questions and answers for each paragraph
+    qa_pairs = []
+    for paragraph in paragraphs:
+        # Ensure that the input is in the correct format (SquadExample or dictionary)
+        input_data = {
+            "question": "What is in this paragraph?",  # You can customize the question here
+            "context": paragraph,
+        }
 
-  return qa_pairs
+        # Use the qa_pipeline with the formatted input
+        questions = qa_pipeline(input_data)
+
+        if questions:
+            # Append the generated answer to the qa_pairs
+            qa_pairs.append({"question": input_data["question"], "answer": questions[0]["answer"]})
+
+    return qa_pairs
 
 # Define a function to display the FAQ
 def display_faq(qa_pairs):
-  """Displays the given FAQ in a Streamlit app.
+    """Displays the given FAQ in a Streamlit app.
 
-  Args:
-    qa_pairs: A list of dictionaries, where each dictionary contains a question and its
-    corresponding answer.
-  """
+    Args:
+        qa_pairs: A list of dictionaries, where each dictionary contains a question and its
+        corresponding answer.
+    """
 
-  # If the FAQ list is empty, display a message
-  if not qa_pairs:
-    st.write("No questions and answers found.")
-  else:
-    # Create a table to display the FAQ
-    table = st.table(headers=["Question", "Answer"])
-    for qa_pair in qa_pairs:
-      table.add_row(qa_pair["question"], qa_pair["answer"])
+    # If the FAQ list is empty, display a message
+    if not qa_pairs:
+        st.write("No questions and answers found.")
+    else:
+        # Create a table to display the FAQ
+        table = st.table(headers=["Question", "Answer"])
+        for qa_pair in qa_pairs:
+            table.add_row(qa_pair["question"], qa_pair["answer"])
 
 if __name__ == "__main__":
-  main()
+    main()
