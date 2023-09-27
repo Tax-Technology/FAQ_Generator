063b4778d2898bf3c24342b74f223dfa527163f6
Tax-Technology <138007036+Tax-Technology@users.noreply.github.com>
2023-09-27 19:15:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,29 +4,29 @@ import pandas as pd
 
 # Function to check if the provided API key is valid
 def is_valid_api_key(api_key):
-    try:
-        openai.api_key = api_key
-        openai.Completion.create(engine="text-davinci-002", prompt="Test request", max_tokens=1)
-        return True
-    except Exception as e:
-        return False
+  try:
+    openai.api_key = api_key
+    openai.Completion.create(engine="text-davinci-002", prompt="Test request", max_tokens=1)
+    return True
+  except Exception as e:
+    return False
 
 # Function to generate FAQ using OpenAI API
 def generate_faq(text, num_faqs, selected_tone):
-    try:
-        prompt = f"Generate {num_faqs} FAQs related to the following text: \"{text}\" with a {selected_tone} tone."
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            max_tokens=200,  # Adjust max_tokens as needed for longer answers
-            n=num_faqs,
-            stop=None
-        )
-        return response.choices
-    except Exception as e:
-        st.error("An error occurred while generating FAQs.")
-        st.exception(e)
-        return []
+  try:
+    prompt = f"Generate {num_faqs} FAQs related to the following text: \"{text}\" with a {selected_tone} tone."
+    response = openai.Completion.create(
+      engine="text-davinci-002",
+      prompt=prompt,
+      max_tokens=200, # Adjust max_tokens as needed for longer answers
+      n=num_faqs,
+      stop=None
+    )
+    return response.choices
+  except Exception as e:
+    st.error("An error occurred while generating FAQs.")
+    st.exception(e)
+    return []
 
 # Create a Streamlit app
 st.title("FAQ Generator")
@@ -45,22 +45,22 @@ text_input = st.text_area("Enter your text here (maximum 5000 words)")
 
 # Define a submit button
 if st.button("Generate FAQs", key="generate_button"):
-    if api_key and is_valid_api_key(api_key) and text_input:
-        with st.spinner("Generating FAQs..."):
-            qa_pairs = generate_faq(text_input, num_faqs, selected_tone)
+  if api_key and is_valid_api_key(api_key) and text_input:
+    with st.spinner("Generating FAQs..."):
+      qa_pairs = generate_faq(text_input, num_faqs, selected_tone)
 
-        # Create a DataFrame for questions and answers
-        data = {"Question": [], "Answer": []}
-        for i, qa in enumerate(qa_pairs):
-            question = qa['text'].strip()
-            answer = qa['text'].strip()
-            data["Question"].append(f"Q{i + 1}: {question}")
-            data["Answer"].append(f"A{i + 1}: {answer}")
-        
-        df = pd.DataFrame(data)
-        
-        # Display questions and answers in a table format
-        st.write("### Generated FAQs")
-        st.table(df)
-    else:
-        st.error("Please enter a valid OpenAI API key and some text before generating FAQs.")
+    # Create a DataFrame for questions and answers
+    data = {"Question": [], "Answer": []}
+    for i, qa in enumerate(qa_pairs):
+      question = qa['text'].strip()
+      answer = qa['text'].strip()
+      data["Question"].append(f"Q{i + 1}: {question}\n")
+      data["Answer"].append(f"A{i + 1}: {answer}\n")
+     
+    df = pd.DataFrame(data)
+     
+    # Display questions and answers in a markdown table
+    st.markdown("### Generated FAQs")
+    st.markdown(df.to_markdown())
+  else:
+    st.error("Please enter a valid OpenAI API key and some text before generating FAQs.")
