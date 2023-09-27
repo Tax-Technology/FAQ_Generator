c1c0dd9d966ab3e9081a7eda36504ab1bdeb1271
Tax-Technology <138007036+Tax-Technology@users.noreply.github.com>
2023-09-27 21:10:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,40 +11,20 @@ def is_valid_api_key(api_key):
     except Exception as e:
         return False
 
-# Function to generate FAQ questions using OpenAI API
-def generate_questions(text, num_questions, selected_tone):
+# Function to generate questions using OpenAI API
+def generate_qa_pairs(text, num_qa_pairs, selected_tone):
     try:
-        prompt = f"Generate {num_questions} questions related to the following text: \"{text}\" with a {selected_tone} tone."
+        prompt = f"Generate {num_qa_pairs} questions and answers related to the following text: \"{text}\" with a {selected_tone} tone."
         response = openai.Completion.create(
             engine="text-davinci-002",
             prompt=prompt,
             max_tokens=200,  # Adjust max_tokens as needed for longer answers
-            n=num_questions,
+            n=num_qa_pairs * 2,  # Generate twice as many completions to pair questions and answers
             stop=None
         )
         return response.choices
     except Exception as e:
-        st.error("An error occurred while generating questions.")
-        st.exception(e)
-        return []
-
-# Function to answer questions using OpenAI API
-def answer_questions(text, questions):
-    try:
-        qa_pairs = []
-        for question in questions:
-            prompt = f"Answer the following question using the provided text: \"{question['text']}\""
-            response = openai.Completion.create(
-                engine="text-davinci-002",
-                prompt=prompt,
-                max_tokens=200,  # Adjust max_tokens as needed for longer answers
-                n=1,
-                stop=None
-            )
-            qa_pairs.append((question['text'], response.choices[0]['text'].strip()))
-        return qa_pairs
-    except Exception as e:
-        st.error("An error occurred while answering questions.")
+        st.error("An error occurred while generating questions and answers.")
         st.exception(e)
         return []
 
@@ -54,8 +34,8 @@ st.title("Question and Answer Generator")
 # Add a text input widget for entering the OpenAI API key
 api_key = st.text_input("Enter your OpenAI API key", type="password")
 
-# Add a slider to select the number of questions to generate (between 1 and 10)
-num_questions = st.slider("Number of Questions to Generate", 1, 10, 5)
+# Add a slider to select the number of question and answer pairs to generate (between 1 and 10)
+num_qa_pairs = st.slider("Number of Question and Answer Pairs to Generate", 1, 10, 5)
 
 # Add a selectbox to choose the tone
 selected_tone = st.selectbox("Select Tone", ["friendly", "professional", "technical"])
@@ -67,16 +47,15 @@ text_input = st.text_area("Enter your text here (maximum 5000 words)")
 if st.button("Generate Questions and Answers", key="generate_button"):
     if api_key and is_valid_api_key(api_key) and text_input:
         with st.spinner("Generating Questions and Answers..."):
-            questions = generate_questions(text_input, num_questions, selected_tone)
-            qa_pairs = answer_questions(text_input, questions)
+            qa_pairs = generate_qa_pairs(text_input, num_qa_pairs, selected_tone)
 
         # Create a DataFrame for questions and answers
         data = {"Question": [], "Answer": []}
-        for qa_pair in qa_pairs:
-            question = qa_pair[0]
-            answer = qa_pair[1]
-            data["Question"].append(question)
-            data["Answer"].append(answer)
+        for i in range(num_qa_pairs):
+            question = qa_pairs[i * 2].text.strip()
+            answer = qa_pairs[i * 2 + 1].text.strip()
+            data["Question"].append(f"Q{i + 1}: {question}")
+            data["Answer"].append(f"A{i + 1}: {answer}")
 
         df = pd.DataFrame(data)
 
