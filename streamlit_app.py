2b7cc0c4ce2adb52751ce7461482dbc72615b9ef
Tax-Technology <138007036+Tax-Technology@users.noreply.github.com>
2023-09-27 17:02:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,30 +2,6 @@ import streamlit as st
 import openai
 import base64
 
-# Create a Streamlit app
-st.title("FAQ Generator")
-
-# Add a text input widget for entering the OpenAI API key
-api_key = st.text_input("Enter your OpenAI API key", type="password")
-
-# Add a slider to select the number of FAQs to generate (between 1 and 10)
-num_faqs = st.slider("Number of FAQs to Generate", 1, 10, 5)
-
-# Add a selectbox to choose the tone
-selected_tone = st.selectbox("Select Tone", ["friendly", "professional", "technical"])
-
-# Add a text area for entering text, with a 5000-word limit
-text_input = st.text_area("Enter your text here (maximum 5000 words)")
-
-# Define a submit button
-if st.button("Generate FAQs", key="generate_button"):
-    if api_key and is_valid_api_key(api_key) and len(text_input.split()) <= 5000:
-        # Generate questions and answers using OpenAI API based on user-selected options
-        qa_pairs = generate_faq(text_input, num_faqs, selected_tone)
-
-        # Display the FAQ
-        display_faq(qa_pairs)
-
 # Function to check if the provided API key is valid
 def is_valid_api_key(api_key):
     try:
@@ -70,5 +46,26 @@ def display_faq(qa_pairs):
         st.write(f"**A:** {qa['answer']}")
         st.write("---")
 
-if __name__ == "__main__":
-    main()
+# Create a Streamlit app
+st.title("FAQ Generator")
+
+# Add a text input widget for entering the OpenAI API key
+api_key = st.text_input("Enter your OpenAI API key", type="password")
+
+# Add a slider to select the number of FAQs to generate (between 1 and 10)
+num_faqs = st.slider("Number of FAQs to Generate", 1, 10, 5)
+
+# Add a selectbox to choose the tone
+selected_tone = st.selectbox("Select Tone", ["friendly", "professional", "technical"])
+
+# Add a text area for entering text, with a 5000-word limit
+text_input = st.text_area("Enter your text here (maximum 5000 words)")
+
+# Define a submit button
+if st.button("Generate FAQs", key="generate_button"):
+    if api_key and is_valid_api_key(api_key) and len(text_input.split()) <= 5000:
+        # Generate questions and answers using OpenAI API based on user-selected options
+        qa_pairs = generate_faq(text_input, num_faqs, selected_tone)
+
+        # Display the FAQ
+        display_faq(qa_pairs)
